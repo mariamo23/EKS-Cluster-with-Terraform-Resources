642988b404bca5f9b091863cde0cfd5d3c6ded0e
mariamo23 <124802455+mariamo23@users.noreply.github.com>
2024-04-03 11:15:41 -0400

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,12 +1,5 @@
-#
-# EKS Cluster Resources
-#  * IAM Role to allow EKS service to manage other AWS services
-#  * EC2 Security Group to allow networking traffic with EKS cluster
-#  * EKS Cluster
-#
-
-resource "aws_iam_role" "tsp-cluster" {
-  name = "tsp-cluster"
+resource "aws_iam_role" "demo" {
+  name = "eks-cluster-demo"
 
   assume_role_policy = <<POLICY
 {
@@ -24,54 +17,24 @@ resource "aws_iam_role" "tsp-cluster" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "tsp-cluster-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.tsp-cluster.name
+  role       = aws_iam_role.demo.name
 }
 
-resource "aws_iam_role_policy_attachment" "tsp-cluster-AmazonEKSServicePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.tsp-cluster.name
-}
-
-resource "aws_security_group" "tsp-cluster" {
-  name        = "tsp-cluster-sg"
-  description = "Cluster communication with worker nodes"
-  vpc_id      = aws_vpc.tsp-vpc.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "tsp-cluster"
-  }
-}
-
-resource "aws_security_group_rule" "tsp-cluster-ingress-workstation-https" {
-  cidr_blocks       = [local.workstation-external-cidr]
-  description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.tsp-cluster.id
-  to_port           = 443
-  type              = "ingress"
-}
-
-resource "aws_eks_cluster" "tsp-cluster" {
-  name     = var.cluster-name
-  role_arn = aws_iam_role.tsp-cluster.arn
+resource "aws_eks_cluster" "demo" {
+  name     = "demo"
+  role_arn = aws_iam_role.demo.arn
 
   vpc_config {
-    security_group_ids = [aws_security_group.tsp-cluster.id]
-    subnet_ids         = aws_subnet.tsp-vpc[*].id
+    subnet_ids = [
+      aws_subnet.private-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id,
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+    ]
   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.tsp-cluster-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.tsp-cluster-AmazonEKSServicePolicy,
-  ]
+  depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
 }
+
